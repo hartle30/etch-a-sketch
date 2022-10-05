a80c40b0e1ba2b3d95f0b92024bec5a97487e647
Adam Hartley <adamdhartley@icloud.com>
2022-10-05 16:41:25 -0400

    Create logic for grid creation

---
+++ script.js
@@ -0,0 +1,27 @@
+function createDiv (num) {
+    const container = document.getElementById('container');
+    const height = Math.floor(960/num) - 2;
+    const width = height;
+    if (num > 100) {
+        return;
+    } else {
+        let numSquare = (num * num);
+        for(i=1; i <= numSquare; ++i) {
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('square');
+            container.appendChild(newDiv);
+        }
+        const squares = document.querySelectorAll('.square');
+        squares.forEach(square => square.setAttribute('style', `height: ${height}px; width: ${width}px;`));
+        container.style.cssText = `grid-template-columns: repeat(${num}, 0fr); grid-template-rows: repeat(${num}, 0fr);`;
+    } 
+}
+
+createDiv(16);
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    const size = prompt('How many rows/columns?');
+    document.getElementById('container').innerHTML = '';
+    createDiv(size);
+});
